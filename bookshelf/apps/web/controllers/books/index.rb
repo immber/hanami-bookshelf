aacb7517fca1cd6f4a25df73f465372fdd8a7c51
immber <jessiekrushing@hotmail.com>
2017-11-14 22:28:11 -0800

    made books apear on the screen

--- bookshelf/apps/web/controllers/books/index.rb
+++ bookshelf/apps/web/controllers/books/index.rb
@@ -2,7 +2,11 @@ module Web::Controllers::Books
   class Index
     include Web::Action
 
+    expose :books
+
     def call(params)
+    	@books = BookRepository.new.all
     end
+
   end
 end
--- bookshelf/spec/web/features/list_books_spec.rb
+++ bookshelf/spec/web/features/list_books_spec.rb
@@ -2,8 +2,16 @@
 require 'features_helper'
 
 describe 'List books' do
-  it 'displays each book on the page' do
-    visit '/books'
+	let(:repository) { BookRepository.new }
+	  before do
+	    repository.clear
+
+	    repository.create(title: 'PoEAA', author: 'Martin Fowler')
+	    repository.create(title: 'TDD',   author: 'Kent Beck')
+	  end
+
+	it 'displays each book on the page' do
+	   visit '/books'
 
     within '#books' do
       assert page.has_css?('.book', count: 2), 'Expected to find 2 books'
